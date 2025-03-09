c78f7143f1d9316422630e26ca8b0e1b08a0234d
NikolajCupan <cupannikolaj@gmail.com>
2025-03-09 12:48:08 +0100

    Added dataset for regression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import GUI.GUI;
 import NeuralNetwork.BuildingBlocks.Batch;
 import NeuralNetwork.NeuralNetwork;
 import Utilities.Factory;
+
+import java.util.Arrays;
 
 public class Main {
     private static void run() {
@@ -49,12 +52,35 @@ public class Main {
         final Batch predictionsFromPredict = neuralNetwork.predict(testInput);
     }
 
+    private static void run4() {
+        final Batch trainInput = Factory.getTrainInputBatchForRegression();
+        final Batch trainTarget = Factory.getTrainTargetBatchForRegression();
+
+        final Batch testInput = Factory.getTestInputBatchForRegression();
+        final Batch testTarget = Factory.getTestTargetBatchForRegression();
+
+
+        final NeuralNetwork neuralNetwork = Factory.getNeuralNetwork4();
+        neuralNetwork.train(trainInput, trainTarget, 1000, Integer.MAX_VALUE, 100, Integer.MAX_VALUE);
+        neuralNetwork.test(testInput, testTarget);
+
+
+        final Batch predicted = neuralNetwork.getPredictedBatch();
+        final GUI gui = new GUI();
+
+        gui.show(Arrays.asList(
+                new GUI.ChartData(testTarget.getColumn(0), "target", 0),
+                new GUI.ChartData(predicted.getColumn(0), "predicted", 0)
+        ));
+    }
+
     public static void main(String[] args) {
         final long startTime = System.currentTimeMillis();
 
         // Main.run();
         // Main.run2();
-        Main.run3();
+        // Main.run3();
+        Main.run4();
 
         final long endTime = System.currentTimeMillis();
         System.out.println("Time needed: " + (endTime - startTime) + " ms");
